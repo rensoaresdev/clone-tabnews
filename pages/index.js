4ebaac51263b082bb0fb937e9ff9fae8deb3f700
rensoaresdev <102915491+rensoaresdev@users.noreply.github.com>
2024-10-08 20:44:55 +0000

    Altera mensagem do arquivo index.js - 2ª vez

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
 
-    return <h1>Teste 2</h1>
-
+    return <h1>Seja bem-vindo!!!  Desejo que tenha uma ótima experiência neste site. Tenha boas interações...</h1>
 }
 
 export default Home;

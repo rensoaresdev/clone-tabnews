b4467aa92d746cf5970c55a2b594a59711723d8a
rensoaresdev <102915491+rensoaresdev@users.noreply.github.com>
2024-10-09 17:58:48 +0000

    Modifica a frase...

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
 
-    return <h1>Seja bem-vindo!!!  Desejo que tenha uma ótima experiência neste site. Tenha boas interações...</h1>
+    return <h1>Seja bem-vindo!!!  Desejo que tenha 
+        uma ótima experiência neste site. Tenha boas interações e se expanda...</h1>
 }
 
 export default Home;

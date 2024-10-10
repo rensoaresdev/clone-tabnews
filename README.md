857ea7e3f117045c2507dc1349ba5d6b85d21c5e
rensoaresdev <102915491+rensoaresdev@users.noreply.github.com>
2024-10-10 02:37:15 +0000

    adiciona scripts Lint:check e Lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto Treinamento Programação do Curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 function Home() {
+  return (
+    <h1>
+      Seja bem-vindo!!! Desejo que tenha uma ótima experiência neste site. Tenha
+      boas interações e se expanda...
+    </h1>
+  );
+}
 
-    return <h1>Seja bem-vindo!!!  Desejo que tenha 
-        uma ótima experiência neste site. Tenha boas interações e se expanda...</h1>
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;

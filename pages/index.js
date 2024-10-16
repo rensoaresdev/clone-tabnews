153a4bd89be76f228373aa3e2a9a8aaa3074f476
rensoaresdev <102915491+rensoaresdev@users.noreply.github.com>
2024-10-16 14:33:09 +0000

    install jest and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,9 @@ function Home() {
   return (
     <h1>
       Seja bem-vindo!!! Desejo que tenha uma ótima experiência neste site. Tenha
-      boas interações e se expanda...
+      boas interações e se expanda ...
     </h1>
   );
-}
-
-function teste2() {
-  console.log("indentação errada");
 }
 
 export default Home;
